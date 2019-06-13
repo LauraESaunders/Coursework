0dfe6a7296a843158e43da0e06b636f064a5e82b
Laura Saunders <lsaunders@farnborough.ac.uk>
2019-06-13 14:26:48 +0100

    Committing to Git

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("My Coursework...");
-        System.out.println("Added another message");
+        System.out.println("Added another message.");
     }
 }
